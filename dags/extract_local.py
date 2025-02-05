c9bf9d5ff1b8cb3538f9e92a0bfacecd464228c6
gustavocaetano <gustavocaetano.mkt@gmail.com>
2025-02-05 16:54:22 -0300

    DAG change

--- dags/extract_local.py
+++ dags/extract_local.py
@@ -1,6 +1,8 @@
 from airflow import DAG
 from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.operators.bash import BashOperator
 from datetime import datetime
+from docker.types import Mount
 
 default_args = {
     'owner': 'airflow',
@@ -17,34 +19,57 @@ with DAG(
     catchup=False,
 ) as dag:
 
+    create_postgres_dir = BashOperator(
+        task_id='create_postgres_dir',
+        bash_command=f'mkdir -p ./project/data/local_data/postgres'
+    )
+
     extract_postgres = DockerOperator(
         task_id='extract_postgres',
-        image='meu-meltano',  # Nome da imagem que você criou
+        image='meu-meltano',
         command='meltano run tap-postgres target-parquet',
         docker_url='unix://var/run/docker.sock',
-        network_mode='host',  # Ajuste conforme necessário
-        volumes=['./project/data/local_data/postgres:/project/data/local_data/'],
+        network_mode='host',
+        mounts=[
+            Mount(
+                source='./etl_project/data/local_data/',
+                target='/project/data/local_data/',
+                type='volume'
+            )
+        ],
         auto_remove=True,
     )
 
     move_postgres_files = BashOperator(
         task_id='move_postgres_files',
-        bash_command=f'mkdir -p ./project/data/local_data/postgres/{{{{ ds_nodash }}}}; mv ./project/data/local_data/postgres/* ./project/data/local_data/postgres/{{{{ ds_nodash }}}}/',
+        bash_command=f'mv ./project/data/local_data/extract/* ./project/data/local_data/postgres/',
+    )
+
+    create_csv_dir = BashOperator(
+        task_id='create_csv_dir',
+        bash_command=f'mkdir -p ./project/data/local_data/csv',
     )
 
     extract_csv = DockerOperator(
         task_id='extract_csv',
-        image='meu-meltano',  # Nome da imagem que você criou
+        image='meu-meltano',
         command='meltano run tap-csv target-parquet',
         docker_url='unix://var/run/docker.sock',
-        network_mode='host',  # Ajuste conforme necessário
-        volumes=['./project/data/local_data/csv:/project/data/local_data/'],
+        network_mode='host',
+        mounts=[
+            Mount(
+                source='./project/data/local_data/',
+                target='/project/data/local_data/',
+                type='volume'
+            )
+        ],
         auto_remove=True,
     )
 
     move_csv_files = BashOperator(
         task_id='move_csv_files',
-        bash_command=f'mkdir -p ./project/data/local_data/csv/{{{{ ds_nodash }}}}; mv ./project/data/local_data/csv/* ./project/data/local_data/csv/{{{{ ds_nodash }}}}/',
+        bash_command=f'mv ./project/data/local_data/extract/* ./project/data/local_data/csv/',
     )
 
-    extract_postgres >> extract_csv
+    create_postgres_dir >> extract_postgres >> move_postgres_files
+    extract_postgres >> create_csv_dir >> extract_csv >> move_csv_files
