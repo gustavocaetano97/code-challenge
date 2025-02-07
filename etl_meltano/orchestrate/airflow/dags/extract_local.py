04f0784e9defec7df443c392574f44977edee473
gustavocaetano <gustavocaetano.mkt@gmail.com>
2025-02-07 20:36:21 -0300

    debug DAG

--- etl_meltano/orchestrate/airflow/dags/extract_local.py
+++ etl_meltano/orchestrate/airflow/dags/extract_local.py
@@ -1,12 +1,17 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+import os
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'retries': 1,
 }
+
+def get_execution_date(**context):
+    return context['execution_date'].strftime("%Y-%m-%d")
 
 with DAG(
     'extract_to_local',
@@ -15,33 +20,22 @@ with DAG(
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
-    extract_csv = BashOperator(
-        task_id='extract_csv_to_parquet',
-        bash_command='cd /path/to/meltano/project; meltano run tap-csv target-parquet',
+
+    get_date = PythonOperator(
+    task_id='get_date',
+    python_callable=get_execution_date,
+    provide_context=True,
+    dag=dag
     )
 
-    move_csv = BashOperator(
-        task_id='move_csv_to_date_folder',
-        bash_command="""
-            date_path=$(date +'%Y-%m-%d')
-            mkdir -p /project/data/local_data/csv/$date_path
-            mv /project/data/local_data/extract/* /project/data/local_data/csv/$date_path/
-        """,
+    extract_csv = BashOperator(
+        task_id='extract_csv_to_parquet',
+        bash_command='export EXECUTION_DATE={{ task_instance.xcom_pull(task_ids="get_date") }} && cd /project && meltano run tap-csv target-parquet',
     )
 
     extract_postgres = BashOperator(
         task_id='extract_postgres_to_parquet',
-        bash_command='cd /path/to/meltano/project; meltano run tap-postgres target-parquet',
+        bash_command='export EXECUTION_DATE={{ task_instance.xcom_pull(task_ids="get_date") }} && cd /project && meltano run tap-postgres target-parquet',
     )
 
-    move_postgres = BashOperator(
-        task_id='move_postgres_to_date_folder',
-        bash_command="""
-            date_path=$(date +'%Y-%m-%d')
-            mkdir -p /project/data/local_data/postgres/$date_path
-            mv /project/data/local_data/extract/* /project/data/local_data/postgres/$date_path/
-        """,
-    )
-
-    extract_csv >> move_csv
-    extract_postgres >> move_postgres
+    get_date >> extract_csv >> extract_postgres
